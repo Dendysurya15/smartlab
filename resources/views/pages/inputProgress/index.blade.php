626abd098e75e04f6c1e987fcb5fdc4e394ec0c4
Dend <dendysurya15@gmail.com>
2023-10-19 12:10:57 +0700

    page input

--- resources/views/pages/inputProgress/index.blade.php
+++ resources/views/pages/inputProgress/index.blade.php
@@ -1,3 +1,254 @@
 <x-app-layout>
-    <h1>Input Progress page</h1>
+    <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+        <div
+            class="col-span-full xl:col-span-6 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
+            <header class="flex px-5 py-4 border-b border-slate-100 dark:border-slate-700">
+                <h2 class="font-bold text-slate-800 dark:text-slate-100">INPUT PENGERJAAN PROGRESS SAMPEL BARU</h2>
+            </header>
+            <div class="p-5">
+                <!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+                <form>
+                    <div class="space-y-12">
+                        <div class="border-b border-gray-900/10 pb-12">
+                            <h2 class="text-base font-semibold leading-7 text-gray-900">Informasi Sampel dan Pelanggan
+                            </h2>
+                            <p class="mt-1 text-sm leading-6 text-gray-600">Peringatan untuk crosscheck ulang seluruh
+                                data yang ingin akan dimasukkan ke sistem!</p>
+
+                            <div class="mt-5 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div class="sm:col-span-2">
+                                    <label for="tanggal_penerimaan"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Tanggal
+                                        Penerimaan</label>
+                                    <div class="mt-2">
+                                        <input type="date" name="tanggal_penerimaan" id="tanggal_penerimaan"
+                                            autocomplete="given-name"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    </div>
+                                </div>
+
+                                <div class="sm:col-span-2">
+                                    <label for="last-name"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Jenis Sampel</label>
+                                    <div class="mt-2">
+                                        <select id="country" name="country" autocomplete="country-name"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                                            <option>United States</option>
+                                            <option>Canada</option>
+                                            <option>Mexico</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+
+
+                                <div class="sm:col-span-2">
+                                    <label for="last-name"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Asal Sampel</label>
+                                    <div class="mt-2">
+                                        <select id="country" name="country" autocomplete="country-name"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                                            <option>Internal</option>
+                                            <option>Eksternal</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="sm:col-span-2">
+                                    <label for="tanggal_penerimaan"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Nomor Kupa</label>
+                                    <div class="mt-2">
+                                        <input type="number" name="tanggal_penerimaan" id="tanggal_penerimaan"
+                                            autocomplete="given-name"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    </div>
+                                </div>
+
+                                <div class="sm:col-span-2">
+                                    <label for="tanggal_penerimaan"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Nomor Lab</label>
+                                    <div class="mt-2">
+                                        <input type="text" name="tanggal_penerimaan" id="tanggal_penerimaan"
+                                            autocomplete="given-name"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    </div>
+                                </div>
+
+                                <div class="sm:col-span-2">
+                                    <label for="tanggal_penerimaan"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Nama Pengirim</label>
+                                    <div class="mt-2">
+                                        <input type="text" name="tanggal_penerimaan" id="tanggal_penerimaan"
+                                            autocomplete="given-name"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    </div>
+                                </div>
+
+                                <div class="sm:col-span-2">
+                                    <label for="tanggal_penerimaan"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Nama Departemen /
+                                        Perusahaan</label>
+                                    <div class="mt-2">
+                                        <input type="text" name="tanggal_penerimaan" id="tanggal_penerimaan"
+                                            autocomplete="given-name"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    </div>
+                                </div>
+
+                                <div class="sm:col-span-2">
+                                    <label for="tanggal_penerimaan"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Kode Sampel</label>
+                                    <div class="mt-2">
+                                        <input type="text" name="tanggal_penerimaan" id="tanggal_penerimaan"
+                                            autocomplete="given-name"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    </div>
+                                </div>
+
+                                <div class="sm:col-span-2">
+                                    <label for="tanggal_penerimaan"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Nomor Surat
+                                    </label>
+                                    <div class="mt-2">
+                                        <input type="text" name="tanggal_penerimaan" id="tanggal_penerimaan"
+                                            autocomplete="given-name"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    </div>
+                                </div>
+
+                                <div class="sm:col-span-2">
+                                    <label for="tanggal_penerimaan"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Estimasi Kupa</label>
+                                    <div class="mt-2">
+                                        <input type="date" name="tanggal_penerimaan" id="tanggal_penerimaan"
+                                            autocomplete="given-name"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    </div>
+                                </div>
+
+                                <div class="sm:col-span-2">
+                                    <label for="last-name"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Tujuan</label>
+                                    <div class="mt-2">
+                                        <select id="country" name="country" autocomplete="country-name"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
+                                            <option>Internal</option>
+                                            <option>Eksternal</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div class="sm:col-span-2">
+                                    <label for="tanggal_penerimaan"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Parameter
+                                        Analisis</label>
+                                    <div class="mt-2">
+                                        <input type="date" name="tanggal_penerimaan" id="tanggal_penerimaan"
+                                            autocomplete="given-name"
+                                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                                    </div>
+                                </div>
+
+                                <div class="col-span-full">
+                                    <label for="cover-photo"
+                                        class="block text-sm font-medium leading-6 text-gray-900">Upload Foto
+                                        Sampel</label>
+                                    <div
+                                        class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                                        <div class="text-center flex flex-col items-center">
+                                            <!-- Conditional SVG or Image -->
+                                            <div id="image-container">
+                                                <svg class="mx-auto h-50 w-50 text-gray-300" viewBox="0 0 24 24"
+                                                    fill="currentColor" aria-hidden="true">
+                                                    <!-- Placeholder SVG content -->
+                                                    <path fill-rule="evenodd"
+                                                        d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z"
+                                                        clip-rule="evenodd" />
+                                                </svg>
+                                            </div>
+                                            <!-- End of Conditional SVG or Image -->
+                                            <div class="mt-4 text-sm leading-6 text-gray-600">
+                                                <label for="file-upload"
+                                                    class="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500">
+                                                    <span>Upload a file</span>
+                                                    <input id="file-upload" name="file-upload" type="file"
+                                                        class="sr-only">
+                                                </label>
+                                                <p class="pl-1">or drag and drop</p>
+                                                <p class="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
+                                                <!-- Remove button -->
+                                                <div class="flex justify-center mt-2">
+                                                    <button id="remove-button" type="button"
+                                                        class="text-sm font-semibold leading-6 text-red-600 cursor-pointer"
+                                                        style="display: none;">Remove Image</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+
+                            </div>
+                        </div>
+
+
+                        <div class="mt-6 flex items-center justify-end gap-x-6">
+                            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+                            <button type="submit"
+                                class="rounded-md bg-emerald-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-emerald-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-600">Simpan
+                                Progress</button>
+                        </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
 </x-app-layout>
+<script>
+    const fileInput = document.getElementById('file-upload');
+    const imageContainer = document.getElementById('image-container');
+    const removeButton = document.getElementById('remove-button');
+
+    fileInput.addEventListener('change', function() {
+        const file = fileInput.files[0];
+        if (file) {
+            if (file.type.startsWith('image/')) {
+                const fileURL = URL.createObjectURL(file);
+                imageContainer.innerHTML = `<img class="mx-auto h-50 w-50" src="${fileURL}" alt="Preview Image">`;
+                removeButton.style.display = 'block';
+            } else {
+                imageContainer.innerHTML = '<p class="text-red-500">Invalid file type. Please select an image.</p>';
+                removeButton.style.display = 'none';
+            }
+        } else {
+            imageContainer.innerHTML = `
+                <svg class="mx-auto h-50 w-50 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
+                </svg>`;
+            removeButton.style.display = 'none';
+        }
+    });
+
+    removeButton.addEventListener('click', function() {
+        fileInput.value = ''; // Clear the file input
+        imageContainer.innerHTML = `
+            <svg class="mx-auto h-50 w-50 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clip-rule="evenodd" />
+            </svg>`;
+        removeButton.style.display = 'none';
+    });
+</script>
