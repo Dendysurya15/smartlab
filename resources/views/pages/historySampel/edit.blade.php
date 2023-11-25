0d162617e95b3c8c2826e1ffd91ddb256d869fd7
Dend <dendysurya15@gmail.com>
2023-11-25 17:49:17 +0700

    Auto stash before merge of "new-input-progress" and "origin/new-input-progress"

--- resources/views/pages/historySampel/edit.blade.php
+++ resources/views/pages/historySampel/edit.blade.php
@@ -1,13 +1,15 @@
 <x-app-layout>
     <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-        <div class="col-span-full xl:col-span-6 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
+        <div
+            class="col-span-full xl:col-span-6 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
             <header class="flex px-5 py-4 bg-slate-800 border-b border-slate-100 dark:border-slate-700">
                 <h2 class="font-bold text-slate-200 dark:text-slate-100">DETAIL PROGRESS SAMPLE {{$sampel->kode_track}}
                 </h2>
             </header>
             @if (session('success'))
 
-            <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+            <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400"
+                role="alert">
                 <span class="font-medium"> {{ session('success') }}</span>
             </div>
             @endif
